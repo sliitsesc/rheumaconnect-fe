5b101cf83f86d4c73f2bd5b034c2b1d72ba94ddf
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-12-28 16:41:46 +0530

    CardsSection Implemented.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import CardsSection from "./components/CardsSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection selectedLang="en" />
+      <CardsSection />
     </div>
   );
 }
