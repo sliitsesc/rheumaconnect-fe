e12f6d654148b97a575b204cb381ac2043571211
Aneeq Shaffy <103246434+gitPirate12@users.noreply.github.com>
2025-01-11 00:17:33 +0530

    feat: implement seek medical help section (#33)
    * basic structure done
    
    * fixed layout
    
    * desktop style done
    
    * minor button size fix
    
    * moile view done
    
    ---------
    
    Co-authored-by: Pawan Senpura <86240715+pawan-live@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection";
+import ContactCard from "./components/ContactCard";
 import MainCategorySection from "./components/MainCategorySection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection selectedLang="en" />
+      <ContactCard />
       <MainCategorySection />
     </div>
   );
