d8551f43e0cca55426ea07bb4bd34294c32a6efc
Sapumal Wijekoon <72787452+saputhebeast@users.noreply.github.com>
2025-01-11 00:24:01 +0530

    feat: implement a to z cards (#34)
    Co-authored-by: Pawan Senpura <86240715+pawan-live@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import HeroSection from "./components/HeroSection";
+import AZCards from "@/app/components/AZCards";
+import translations from "@/app/data/translations.json";
+import { Translations } from "@/app/types";
 import ContactCard from "./components/ContactCard";
 import MainCategorySection from "./components/MainCategorySection";
+const translationsData: Translations = translations;
 
 export default function Home() {
   return (
@@ -8,6 +12,7 @@ export default function Home() {
       <HeroSection selectedLang="en" />
       <ContactCard />
       <MainCategorySection />
+      <AZCards azCards={translationsData["si"]["az-cards"]} />
     </div>
   );
 }
