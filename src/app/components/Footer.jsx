d47d3b43492b72770eca1ebcae2f52cbad96489f
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2025-01-11 02:14:07 +0530

    Contents of footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <h3>RheumaConnect</h3>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
+        <br />
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore.{" "}
+      </p>
+      <h3>Links</h3>
+      <ul>
+        <li>Tincidunt amet</li>
+        <li>Himenaeos</li>
+        <li>Mollis consequat</li>
+        <li>Facilisi diam</li>
+        <li>inceptos blandit</li>
+      </ul>
+      <h3>Links</h3>
+      <ul>
+        <li>Tincidunt amet</li>
+        <li>Himenaeos</li>
+        <li>Mollis consequat</li>
+        <li>Facilisi diam</li>
+        <li>inceptos blandit</li>
+      </ul>
+      <h3>Connect</h3>
+      <span>453 Prosacco Summit, Port Goldenland, ID 80140</span>
+      <span>+91662782769872</span>
+      <span>+91662782769872</span>
+    </>
+  );
+};
+
+export default Footer;
