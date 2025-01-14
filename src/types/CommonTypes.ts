6c5b4b750c50e06f6365134269c52f6b84be9693
pawan-live <pawan.senpura@gmail.com>
2025-01-14 05:42:03 +0530

    refactor: update data fetch logic + add types

--- src/types/CommonTypes.ts
+++ src/types/CommonTypes.ts
@@ -19,3 +19,24 @@ export interface ButtonType {
   buttonText: string;
   buttonLink?: string;
 }
+
+//#region Categories
+export interface ApiResponse<T> {
+  data: T[];
+}
+
+export interface Category {
+  id: string;
+  documentId: string;
+  name: string;
+  subtitle: string;
+  slug: string;
+  subcategories: Subcategory[];
+}
+
+export interface Subcategory {
+  id: string;
+  name: string;
+  slug: string;
+}
+//#endregion
