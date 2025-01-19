3f629d8a24d8004f3e9c9ebefdf31313239c877f
pawan-live <pawan.senpura@gmail.com>
2025-01-20 02:05:23 +0530

    refactor: individual article card

--- src/app/components/Articles/ArticleCard.tsx
+++ src/app/components/Articles/ArticleCard.tsx
@@ -1,55 +1,33 @@
+import { Article } from "@/types/CommonTypes";
 import Image from "next/image";
 import Link from "next/link";
 
-interface ArticleCardProps {
-  categorySlug: string;
-  subcategorySlug: string;
-  articles: Array<{
-    description: string;
-    id: number;
-    title: string;
-    imageUrl: string;
-    slug: string;
-  }>;
-}
-
 export default function ArticleCard({
-  articles,
+  title,
+  slug,
+  description,
   categorySlug,
   subcategorySlug,
-}: ArticleCardProps) {
+  thumbnailImage,
+}: Article) {
   return (
-    <section className="p-6">
-      {/* <h1 className="text-3xl font-bold text-center text-black mb-4">{name}</h1>
-      <p className="text-center text-gray-600 mb-8">{subtitle}</p> */}
-
-      {/* Responsive grid layout */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {articles.map((article) => (
-          <div
-            key={article.id}
-            className="bg-gradient-to-r from-[#eaf5ff98] to-[#f4f9ff86] p-6 rounded-xl">
-            <Image
-              className="w-full h-[150px] object-cover object-center rounded-lg"
-              src={article.imageUrl || "/placeholder.png"} // Fallback image if none provided
-              alt={article.title}
-              width={1000}
-              height={1000}
-            />
-            <h2 className="font-semibold text-2xl text-black mt-4">
-              {article.title}
-            </h2>
-            <p className="text-black m-1">
-              {article.description.slice(0, 100)}...
-            </p>
-            <Link href={`/${categorySlug}/${subcategorySlug}/${article.slug}`}>
-              <button className="bg-transparent text-[#2F7CC4] border-2 border-[#2F7CC4] w-full h-10 rounded-[12px] text-[16px] font-medium hover:bg-[#276ca3] hover:text-white transition flex items-center justify-center">
-                Explore more
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div
+      key={title}
+      className="bg-gradient-to-r from-[#eaf5ff98] to-[#f4f9ff86] p-6 rounded-xl">
+      <Image
+        className="w-full h-[150px] object-cover object-center rounded-lg"
+        src={thumbnailImage?.url}
+        alt={title}
+        width={1000}
+        height={600}
+      />
+      <h2 className="font-semibold text-2xl text-black mt-4">{title}</h2>
+      <p className="text-black m-1">{description}</p>
+      <Link href={`/${categorySlug}/${subcategorySlug}/${slug}`}>
+        <button className="bg-transparent text-[#2F7CC4] border-2 border-[#2F7CC4] w-full h-10 rounded-[12px] text-[16px] font-medium hover:bg-[#276ca3] hover:text-white transition flex items-center justify-center">
+          Explore more
+        </button>
+      </Link>
+    </div>
   );
 }
