323ad3f7827e0a5bde162c1ff2edffce7ae31a18
pawan-live <pawan.senpura@gmail.com>
2025-01-24 00:38:46 +0530

    chore: add error handling

--- src/lib/helpers/dataFetchHelper.ts
+++ src/lib/helpers/dataFetchHelper.ts
@@ -2,6 +2,10 @@ import { API_URL } from "@/config";
 
 export async function getData(url: string) {
   try {
+    if (!API_URL) {
+      throw new Error("API_URL is not defined");
+    }
+
     const response = await fetch(API_URL + url);
     const data = await response.json();
     return data;
